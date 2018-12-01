2a86e9c49a345159da74de731422be3976d9719e
Kah Yap <kahyap13@gmail.com>
2018-12-01 07:32:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,32 @@
+require 'pry'
+
 def consolidate_cart(cart)
   # code here
+  items = Hash.new(0)
+  cart.each do |item|
+    item.each do |name, details|
+      items[name] = details
+      items[name][:count] = 0
+    end
+  end
+
+  cart.each do |item|
+    item.each do |name, details|
+      items[name][:count] += 1
+    end
+  end
+  return items
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  cart.each do |items|
+    items.each do |item_name, info|
+      info.each do |k, v|
+        binding.pry
+      end
+    end
+  end
 end
 
 def apply_clearance(cart)
